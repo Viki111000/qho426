5cdc1eac6eaaa8ed0939c8930334fab5fabef367
Viki111000 <viktorijalaryionava@yahoo.com>
2023-07-04 16:37:05 +0100

    Week 2 seminar

---
+++ demos/W2_happy.py
@@ -0,0 +1,10 @@
+h =input("Are you happy?")
+k =input("Do you know it?")
+
+if h. lower() == "yes" and k. lower() == "yes":
+    print("Clap your hands")
+elif h. lower() == "yes" and k. lower() == "no":
+    print( "Appreciate what you have, you ungreatful...")
+else:
+    print( "Seek help - let's talk")
+print( "Glad you are here")
---
+++ demos/decisions/and_operator.py
@@ -0,0 +1,7 @@
+noise = input("What did i hear?")
+sight = input("What did i see?")
+
+if noise == "grr" and sight == "two red eyes":
+    print("There is a scary creature. I should get out of here!")
+else:
+    print("I am little scared but i will continue.")
